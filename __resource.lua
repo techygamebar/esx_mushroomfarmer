f9ff56785222ccd75f715dea74a83c59626c9ed1
Techy Game Bar <65540686+techygamebar@users.noreply.github.com>
2021-04-25 17:37:17 +0530

    Created resource.lua

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description "mushroom farming script"
+
+
+client_scripts{ 
+ 	"src/client.lua"
+}
+
+server_scripts{ 
+  	"src/server.lua"
+}
